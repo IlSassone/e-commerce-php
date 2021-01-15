142988d44d4400ca216bff551f3e4fe061f8bfbc
Mattia Ferrari <mattia2002.ferrari@gmail.com>
2021-01-15 11:17:00 +0100

    initial commit

---
+++ components/connessione.php
@@ -0,0 +1,27 @@
+<?php
+
+    $conn = mysqli_connect("localhost","root","","verificheinfo");
+
+    if (($conn == false) || ($conn->connect_errno)) {
+        echo "Errore in connessione MySQL()";
+        exit();
+    }
+    
+    function eseguiquery($sql)
+    {
+        global $conn;
+        $resultset = $conn->query($sql);
+        $righe = mysqli_fetch_all($resultset, MYSQLI_ASSOC);
+        return $righe;
+    }
+
+    function insert($sql)
+    {
+        global $conn;
+        mysqli_query($conn, $sql);
+    }
+
+
+
+?>
+
